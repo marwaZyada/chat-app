7b184e81aea2e13e69ce272f3d6a8fa1390a1562
marwaZyada <101737141+marwaZyada@users.noreply.github.com>
2026-06-21 02:03:44 +0300

    save token interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { tokenInterceptor } from './core/interceptors/token-interceptor';
+import { tokenInterceptor } from './core/interceptors/token/token-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
